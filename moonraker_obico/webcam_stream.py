c6f5a55da2b0418930103710ddd5305d0ebf5454
Wiktor Wołk <wiktorwolk91@gmail.com>
2022-08-01 17:21:44 +0200

    add normal h264

--- moonraker_obico/webcam_stream.py
+++ moonraker_obico/webcam_stream.py
@@ -104,7 +104,7 @@ class WebcamStreamer:
         def h264_encoder():
             test_video = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin', 'test-video.mp4')
             FNULL = open(os.devnull, 'w')
-            for encoder in ['h264_omx', 'h264_v4l2m2m']:
+            for encoder in ['h264_omx', 'h264_v4l2m2m','h264']:
                 ffmpeg_test_proc = psutil.Popen('{} -re -i {} -pix_fmt yuv420p -vcodec {} -an -f rtp rtp://localhost:8014?pkt_size=1300'.format(FFMPEG, test_video, encoder).split(' '), stdout=FNULL, stderr=FNULL)
                 if ffmpeg_test_proc.wait() == 0:
                     return encoder
